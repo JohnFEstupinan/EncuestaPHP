2b1f90bc13fd249d7c7f4109a41951f4269fb0c5
John Freddy Estupiñan Gomez <johnf-estupinang@unilibre.edu.co>
2019-04-30 14:16:07 -0500

    Se adjunta archivo de configuracion y se modifica la conexion

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+        Constantes para la Conexion
+    */
+    define("BaseDeDatos","bdencuesta");
+    define("Usuario","root");
+    define("Contrasena","");
+    define("Servidor","localhost");
+?>
